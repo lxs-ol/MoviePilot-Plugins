9f991cc79f7ccc32a3936bf0edb0c6f535759488
李先生ol <79429324+lxs-ol@users.noreply.github.com>
2026-05-27 21:55:45 +0800

    Update __init__.py

--- plugins.v2/mediacovergenerator/__init__.py
+++ plugins.v2/mediacovergenerator/__init__.py
@@ -54,7 +54,7 @@ class MediaCoverGenerator(_PluginBase):
     # 插件图标
     plugin_icon = "https://raw.githubusercontent.com/lxs-ol/MoviePilot-Plugins/main/icons/emby.png"
     # 插件版本
-    plugin_version = "0.9.6"
+    plugin_version = "0.9.7"
     # 插件作者
     plugin_author = "lxsol"
     # 作者主页
